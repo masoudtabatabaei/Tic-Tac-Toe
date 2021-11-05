a2ae5c69c2f110a18d249d0668fb4cfcc66033af
Masoud <masoudtabatabaei20@gmail.com>
2021-11-05 13:49:19 +0330

    implement get random integer function

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ const winCombination = [
     [1,4,7] ,
     [2,5,8] ,
     [0,4,8] ,
-    [2,4,6] 
+    [2,4,6]
 ];
+
+// get random integer include min and max and between range
+function getRandomItemFromArray(min , max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
