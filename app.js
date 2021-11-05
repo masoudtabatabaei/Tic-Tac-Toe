4949d21f5792b14de1c9ba250693354f04355c5c
Masoud <masoudtabatabaei20@gmail.com>
2021-11-05 16:46:56 +0330

    implement human move and computer move

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let origBoard;
+const origBoard = origBoard = Array.from(Array(9).keys());
 const humanPlayer = "X";
 const computerPlayer = "O";
 const winCombination = [
@@ -12,10 +12,41 @@ const winCombination = [
     [2,4,6]
 ];
 
-// get random integer include min and max and between range
+const cells = document.querySelectorAll(".cell");
+
+startGame();
+
+function startGame() {
+    document.querySelector(".end_game").style.display = "none";
+    console.log(origBoard);
+    for(let i = 0; i < cells.length ; i++) {
+        cells[i].addEventListener("click" , function(e) {
+            if(origBoard.includes(+e.target.id)) {
+                e.target.innerText = humanPlayer;
+                let selectedCell = origBoard.indexOf(+e.target.id);
+                origBoard.splice(selectedCell , 1);
+
+                if(origBoard.length >= 1) {
+                    computerPlayerMove();
+                }
+            } else {
+                alert("Incorrect Move!");
+            }
+        });
+    }
+}
+
+function computerPlayerMove() {
+    let selectPointIndex = getRandomItemFromArray(0 , origBoard.length);
+    document.getElementById(origBoard[selectPointIndex]).innerText = computerPlayer;
+    origBoard.splice(selectPointIndex , 1);
+}
+
+
+// get random integer include min and not include max and between range
 function getRandomItemFromArray(min , max){
     min = Math.ceil(min);
     max = Math.floor(max);
 
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
