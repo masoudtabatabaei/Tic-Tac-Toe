aa9060acfe0b268b44147f1a7e6864e67b6dd8e9
Masoud <masoudtabatabaei20@gmail.com>
2021-11-05 13:32:27 +0330

    add winner combination matrix

---
+++ app.js
@@ -0,0 +1,13 @@
+let origBoard;
+const humanPlayer = "X";
+const computerPlayer = "O";
+const winCombination = [
+    [0,1,2] ,
+    [3,4,5] ,
+    [6,7,8] ,
+    [0,3,6] ,
+    [1,4,7] ,
+    [2,5,8] ,
+    [0,4,8] ,
+    [2,4,6] 
+];
